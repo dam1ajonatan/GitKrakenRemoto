f6a2150adde6ace626469e10f18aaf8b20124b7c
Jonatan León Caparrós <dam1ajonatanleoncaparros@iesantoniogala.es>
2021-04-07 12:48:27 +0200

    Cambio de tilde

--- src/programacion/remoto/HolaMundo.java
+++ src/programacion/remoto/HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo 2");
 		
-		System.out.println("Mi nombre es Jonatan LeÃ³n");
+		System.out.println("Mi nombre es Jonatan León");
 	}
 
 }
